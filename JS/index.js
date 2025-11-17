9bd3ffcd4e0055d6cefe59409269763a4f46b5e8
Pixaiti <20251AC0080013@estudantes.ifpr.edu.br>
2025-11-17 07:22:52 -0300

    acabou

--- JS/index.js
+++ JS/index.js
@@ -219,14 +219,4 @@ swapBtn.addEventListener("click", () => {
 });
 
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    }
-  });
-});
 
-const elements = document.querySelectorAll('.reveal');
-
-elements.forEach(el => observer.observe(el));
